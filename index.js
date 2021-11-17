08454f2b8ccdbb9e9586d59098356592cb4e2bda
shrishti2101 <shrishtijain2101@gmail.com>
2021-11-18 00:19:20 +0530

    mongoDB

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require ("express");
-
+const mongoose =require ("mongoose");
+require ('dotenv').config();
 //database
 const Database = require("./database");
 
+
+
+mongoose.connect(
+    process.env.MONGO_URI,
+    {
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    // useFindAndModify:false,
+    // useCreateIndex:true,  
+   })
+   .then(()=> console.log('connection established'))
+   .catch((err)=>{
+        console.log(err);
+    });
 //initialisation
 const OurApp = express();
 
